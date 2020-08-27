f20a1cb2db100a9487584eeff10fe3d6059a82ff
Trbuhozborac <nemanjapipi@hotmail.com>
2020-08-27 17:06:27 +0200

    Menu Created

---
+++ Dan_LVII_Nemanja_Pilipovic/Zadatak_1/Models/Application.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace Zadatak_1.Models
+{
+    class Application
+    {
+        #region Functions
+
+        public void Start()
+        {
+            Menu();
+        }
+
+        private void Menu()
+        {
+            Console.WriteLine("Welcome User!");
+            Console.WriteLine("Enter one of the numbers from menu:");
+            Console.WriteLine("1) Show all Items");
+            Console.WriteLine("2) Item price modification");
+            Console.WriteLine("3) Add new Item");
+            Console.WriteLine("4) Buy");
+            Console.WriteLine("0) Close the app");
+            string userAnswer = Console.ReadLine();
+            if(int.TryParse(userAnswer, out int answer))
+            {
+                switch (answer)
+                {
+                    case 1:
+                        Console.WriteLine("All Items");
+                        break;
+                    case 2:
+                        Console.WriteLine("Price Modification");
+                        break;
+                    case 3:
+                        Console.WriteLine("Add New Item");
+                        break;
+                    case 4:
+                        Console.WriteLine("Buy");
+                        break;
+                    case 0:
+                        Console.WriteLine("Closing the app...");
+                        Thread.Sleep(2000);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter of the numbers from menu");
+                        Menu();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter one of the numbers from menu");
+                Menu();
+            }
+        }
+
+        #endregion
+
+    }
+}
--- Dan_LVII_Nemanja_Pilipovic/Zadatak_1/Program.cs
+++ Dan_LVII_Nemanja_Pilipovic/Zadatak_1/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Zadatak_1
+﻿namespace Zadatak_1
 {
     class Program
     {
